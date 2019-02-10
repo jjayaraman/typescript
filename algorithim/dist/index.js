6e93e76912a74eefc07b1f3b4b78d963a2d7d841
Jayakumar Jayaraman <india.jai@gmail.com>
2019-02-10 21:08:25 +0000

    refcatored

--- algorithim/dist/index.js
+++ algorithim/dist/index.js
@@ -4,35 +4,35 @@
  *
  *
  */
-var ArraysEx = /** @class */ (function () {
-    function ArraysEx() {
-    }
-    ArraysEx.prototype.sortAscending = function (array) {
-        for (var i = 0; i < array.length; i++) {
-            for (var j = i + 1; j < array.length; j++)
+class ArraysEx {
+    sortAscending(array) {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i + 1; j < array.length; j++)
                 if (array[i] > array[j]) {
-                    var temp = array[i];
+                    let temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
         }
         return array;
-    };
-    ArraysEx.prototype.sortDescending = function (array) {
-        for (var i = 0; i < array.length; i++) {
-            for (var j = i + 1; j < array.length; j++)
+    }
+    sortDescending(array) {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = i + 1; j < array.length; j++)
                 if (array[i] < array[j]) {
-                    var temp = array[i];
+                    let temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
         }
         return array;
-    };
-    ArraysEx.prototype.test = function () {
+    }
+    hello() {
+        console.log('hello..');
+    }
+    test() {
         console.log('test..');
-    };
-    return ArraysEx;
-}());
+    }
+}
 console.log("Res : ", ArraysEx.prototype.sortAscending([2, 6, 3, 8, 7, 4, 5]));
 console.log("Res : ", ArraysEx.prototype.sortDescending([2, 6, 3, 8, 7, 4, 5]));
