d972cad46fa123240cadcb49df1184c1311c43a1
Jayakumar Jayaraman <india.jai@gmail.com>
2019-02-10 23:22:17 +0000

    update

--- algorithim/src/string_dash.ts
+++ algorithim/src/string_dash.ts
@@ -1,13 +1,14 @@
 class StringDash {
 
-    numbers = ['3','2','6','4','7','8'];
+    numbers = [3,2]
+    //,'6','4','7','8'];
 
     applyDash() {
 
         this.numbers.forEach(n => {
-            if(parseInt(n) % 2 === 0) {
+            if(n % 2 === 0) {
                 const index = this.numbers.indexOf(n);
-                this.numbers.fill('_', index, index+1);
+                this.numbers.fill(0, index, index+1);
             }
         });
         console.log(this.numbers);
